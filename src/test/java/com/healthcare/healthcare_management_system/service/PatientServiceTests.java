9e044dfba31c9a507e94b1514e5c1374878fd1bd
Anup Gupta <anuup22.cs@gmail.com>
2025-01-31 12:52:52 +0530

    feat: add unit tests for PatientService to validate patient addition functionality

---
+++ src/test/java/com/healthcare/healthcare_management_system/service/PatientServiceTests.java
@@ -0,0 +1,41 @@
+package com.healthcare.healthcare_management_system.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.healthcare.healthcare_management_system.model.Patient;
+import com.healthcare.healthcare_management_system.repository.PatientRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class PatientServiceTests {
+  @Mock
+  PatientRepository patientRepository;
+
+  @InjectMocks
+  PatientService patientService;
+
+  @Test
+  public void addPatient_returnsSavedPatient() {
+    // Arrange
+    Patient patient = Patient.builder()
+        .id(1L)
+        .firstName("Firstname")
+        .lastName("Lastname")
+        .email("patient@gmail.com")
+        .phoneNumber("1234567890")
+        .medicalHistory("Medical history")
+        .allergies("Allergies").build();
+
+    // Act
+    Mockito.when(patientRepository.save(patient)).thenReturn(patient);
+    Patient savedPatient = patientService.addPatient(patient);
+
+    // Assert
+    Assertions.assertThat(savedPatient).isNotNull();
+  }
+}
